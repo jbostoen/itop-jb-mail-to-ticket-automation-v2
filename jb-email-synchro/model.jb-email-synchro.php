305fc6eaf276cfd28d898528c83b70e5011b8fc9
jbostoen <->
2021-07-16 11:14:52 +0200

    * set version number

--- jb-email-synchro/model.jb-email-synchro.php
+++ jb-email-synchro/model.jb-email-synchro.php
@@ -3,7 +3,7 @@
 /**
  * @copyright   Copyright (c) 2019-2021 Jeffrey Bostoen
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2.6.210406
+ * @version     2.6.210716
  *
  * PHP Data Model definition file
  */
