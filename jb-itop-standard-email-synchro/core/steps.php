58878de9eb39a235fc810c756fae45cbfb1d4014
jbostoen <6421683+jbostoen@users.noreply.github.com>
2023-12-06 18:50:06 +0100

    * Bump version

--- jb-itop-standard-email-synchro/core/steps.php
+++ jb-itop-standard-email-synchro/core/steps.php
@@ -3,7 +3,7 @@
 /**
  * @copyright   Copyright (c) 2019-2023 Jeffrey Bostoen
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2.7.230501
+ * @version     2.7.231206
  *
  * Policy interface definition and some classes implementing it.
  * 
