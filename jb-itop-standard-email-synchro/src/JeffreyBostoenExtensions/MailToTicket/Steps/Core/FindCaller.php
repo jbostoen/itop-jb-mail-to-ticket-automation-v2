5243aa8d4a7e14647ec3b189728cbe426194eb82
Jeffrey Bostoen <support@jeffreybostoen.be>
2026-08-26 09:21:12 +0200

    * Bug: SPF/DKIM softfail bypass (#204) not applied to FindCaller.php and UpdateCallerAttributes.php #215

--- jb-itop-standard-email-synchro/src/JeffreyBostoenExtensions/MailToTicket/Steps/Core/FindCaller.php
+++ jb-itop-standard-email-synchro/src/JeffreyBostoenExtensions/MailToTicket/Steps/Core/FindCaller.php
@@ -94,7 +94,7 @@ abstract class FindCaller extends Base {
 
 							case 'fallback_create_person':
 
-								if(preg_match('/\b(spf|dkim)=fail\b/i', $oRawEmail->GetHeader('authentication-results'))) {
+								if(preg_match('/\b(spf|dkim)=(soft)?fail\b/i', $oRawEmail->GetHeader('authentication-results'))) {
 
 									static::Trace("Refusing to create a Person for '{$sCallerEmail}': the receiving mail server reported a failed SPF/DKIM check (Authentication-Results) for this message.");
 									static::HandleViolation();
--- jb-itop-standard-email-synchro/src/JeffreyBostoenExtensions/MailToTicket/Steps/UpdateCallerAttributes.php
+++ jb-itop-standard-email-synchro/src/JeffreyBostoenExtensions/MailToTicket/Steps/UpdateCallerAttributes.php
@@ -46,7 +46,7 @@ abstract class UpdateCallerAttributes extends Base {
 
 		$oRawEmail = ProcessingHelper::GetRawMail();
 
-		if(preg_match('/\b(spf|dkim)=fail\b/i', $oRawEmail->GetHeader('authentication-results'))) {
+		if(preg_match('/\b(spf|dkim)=(soft)?fail\b/i', $oRawEmail->GetHeader('authentication-results'))) {
 			static::Trace(".. Refusing to update Person::{$oCaller->GetKey()}'s attributes: the receiving mail server reported a failed SPF/DKIM check (Authentication-Results) for this message.");
 			return;
 		}
