9a947de51796792a761c16755fce70ceeeb9d9ae
Jeffrey Bostoen <support@jeffreybostoen.be>
2026-09-10 09:15:43 +0200

    * Bug: Fatal error on undeclared static property Logger::$m_oFileLog #289

--- jb-email-synchro/src/JeffreyBostoenExtensions/MailToTicket/Logger.php
+++ jb-email-synchro/src/JeffreyBostoenExtensions/MailToTicket/Logger.php
@@ -19,6 +19,13 @@ class Logger extends LogAPI {
 	const CHANNEL_DEFAULT = 'MailToTicket';
 	const LEVEL_DEFAULT = self::LEVEL_TRACE;
 
+	// - Untyped, deliberately: matches every other LogAPI subclass in core (IssueLog, ToolsLog,
+	//   SetupLog, DeadLockLog, ExceptionLog all declare this the same way). LogAPI::Enable()
+	//   assigns a FileLog *object* here (static::$m_oFileLog = new FileLog($sTargetFile)) - a
+	//   typed declaration here is a type mismatch waiting to surface as soon as Enable() is
+	//   actually called.
+	protected static $m_oFileLog = null;
+
 	/**
 	 * @inheritDoc
 	 */
