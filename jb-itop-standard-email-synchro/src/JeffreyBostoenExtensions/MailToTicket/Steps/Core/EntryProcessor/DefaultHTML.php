99f6604013b4480a3926d469c09ae404d5ef7df7
Jeffrey Bostoen <support@jeffreybostoen.be>
2026-09-12 10:24:13 +0200

    * Bump version.

--- jb-itop-standard-email-synchro/src/JeffreyBostoenExtensions/MailToTicket/Steps/Core/EntryProcessor/DefaultHTML.php
+++ jb-itop-standard-email-synchro/src/JeffreyBostoenExtensions/MailToTicket/Steps/Core/EntryProcessor/DefaultHTML.php
@@ -2,7 +2,7 @@
 /**
  * @copyright   Copyright (c) 2020-2026 Jeffrey Bostoen
  * @license     See license.md
- * @version     3.2.260711
+ * @version     3.2.260912
  */
 
 namespace JeffreyBostoenExtensions\MailToTicket\Steps\Core\EntryProcessor;
